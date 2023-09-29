059be59d61bd111258a86b0eb505ac024eb784ba
Ramalingam.s <139736322+ramrvscasmca23@users.noreply.github.com>
2023-09-29 14:43:42 +0530

    Coding activity8.1 Solution

---
+++ Week_8_Asynchronous_Code/Coding_Activity/Coding_Activity_8.1/coding actvity 8.1.js
@@ -0,0 +1,6 @@
+
+https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=YOUR-API-KEY-HERE
+https://api.openweathermap.org/data/2.5/weather?q=Houston&APPID=YOUR-API-KEY-HERE
+https://api.openweathermap.org/data/2.5/weather?q=Lisbon&APPID=YOUR-API-KEY-HERE
+https://api.openweathermap.org/data/2.5/weather?q=Budapest&APPID=YOUR-API-KEY-HERE
+
